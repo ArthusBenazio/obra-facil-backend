f33415fc11e6a46c2f9bb9b46b61c5c0589c4819
Arthus Benazio <arthusbenazio@gmail.com>
2025-01-14 16:27:14 -0300

    refactor: change project controller with add dependecy date-fns

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
 import { UnauthorizedError } from "../helpers/api-erros";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { projectResponseSchema, projectSchema } from "../schemas/projectSchemas";
+import {
+  projectResponseSchema,
+  projectSchema,
+} from "../schemas/projectSchemas";
 import { projectService } from "../services/projectService";
 import { FastifyTypedInstance } from "../utils/fastifyTypedInstance";
+import { parse } from "date-fns";
 
 interface User {
   userId: string;
@@ -13,9 +17,9 @@ interface User {
 export function projectController(server: FastifyTypedInstance) {
   server.post(
     "/obras",
-    { 
+    {
       preHandler: [authMiddleware],
-      schema: { 
+      schema: {
         body: projectSchema,
         response: {
           201: projectResponseSchema,
@@ -26,13 +30,18 @@ export function projectController(server: FastifyTypedInstance) {
     async (request, reply) => {
       const user = request.user as User;
 
-      console.log("user", user);
-
       if (!user) {
         throw new UnauthorizedError("Usuário não autenticado.");
       }
 
       const body = projectSchema.parse(request.body);
+
+      const startDate = parse(body.start_date, "dd/MM/yyyy", new Date());
+      const expectedEndDate = parse(
+        body.expected_end_date,
+        "dd/MM/yyyy",
+        new Date()
+      );
 
       const newProject = await projectService.createProject({
         name: body.name,
@@ -40,8 +49,8 @@ export function projectController(server: FastifyTypedInstance) {
         responsible: body.responsible,
         engineer: body.engineer,
         crea_number: body.crea_number,
-        start_date: new Date(body.start_date),
-        expected_end_date: new Date(body.expected_end_date),
+        start_date: startDate,
+        expected_end_date: expectedEndDate,
         status: body.status,
         address: body.address,
         estimated_budget: body.estimated_budget,
@@ -58,30 +67,31 @@ export function projectController(server: FastifyTypedInstance) {
 
   server.get(
     "/obras",
-    { 
+    {
       preHandler: [authMiddleware],
       schema: {
         description: "Retorna todas as obras de um usuário ou empresa",
         tags: ["Obras"],
         response: {
           200: z.array(projectResponseSchema),
-        }
-      }
+        },
+      },
     },
     async (request, reply) => {
-  
       const user = request.user as User;
-  
+
       if (!user) {
         throw new UnauthorizedError("Usuário não autenticado.");
       }
-  
+
       const { userId, companyId } = user;
-  
-      const projects = await projectService.getAllProjects(userId, companyId ?? "");
-  
+
+      const projects = await projectService.getAllProjects(
+        userId,
+        companyId ?? ""
+      );
+
       reply.status(200).send(projects);
     }
   );
-  
 }
