d923b5590147ed5672b6c8819ff139f115ee8b9e
Arthus Benazio <arthusbenazio@gmail.com>
2025-01-30 14:50:34 -0300

    feat: create table weather and exclude column weather from constructio_log

---
+++ prisma/migrations/20250128170904_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `weather` on the `construction_log` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "construction_log" DROP COLUMN "weather";
