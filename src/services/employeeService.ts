5f9fb9dce6248043e446c3918cdd1705981db8b3
Arthus Benazio <arthusbenazio@gmail.com>
2025-03-19 13:41:35 -0300

    refactor: add get report hours worked

--- src/services/employeeService.ts
+++ src/services/employeeService.ts
@@ -2,6 +2,12 @@ import { Employee } from "../entities/employee";
 import { User } from "../entities/user";
 import { prisma } from "../lib/prisma";
 
+type ReportItem = {
+  name: string;
+  daily_rate: number;
+  work_days: { hours_worked: number; created_at: Date }[];
+};
+
 export const employeeService = {
   async createEmployee(data: {
     name: string;
@@ -24,7 +30,7 @@ export const employeeService = {
         pix_key: data.pix_key,
         company: {
           connect: {
-            id: data.company_id, 
+            id: data.company_id,
           },
         },
       },
@@ -34,14 +40,11 @@ export const employeeService = {
   async getAllEmployees(user: User): Promise<Employee[]> {
     const employees = await prisma.employee.findMany({
       where: {
-        AND: [
-          { company_id: user.companyId },
-        ],
+        AND: [{ company_id: user.companyId }],
       },
     });
     return employees;
   },
-  
 
   async getEmployeeById(id: string): Promise<Employee | null> {
     return prisma.employee.findUnique({
@@ -66,5 +69,57 @@ export const employeeService = {
         id,
       },
     });
+  },
+
+  async getReportHoursWorked(
+    start_date?: string,
+    end_date?: string
+  ): Promise<any> {
+    let startDate: Date | undefined;
+    let endDate: Date = new Date();
+
+    if (start_date && end_date) {
+      startDate = new Date(start_date);
+      endDate = new Date(end_date);
+    } 
+    const report = await prisma.construction_log_employee.findMany({
+      where: {
+        created_at: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      select: {
+        employee: {
+          select: {
+            name: true,
+            daily_rate: true,
+          },
+        },
+        hours_worked: true,
+        created_at: true,
+      },
+      orderBy: {
+        created_at: "asc",
+      }
+    });
+
+    const groupedReport = report.reduce<ReportItem[]>((acc, entry) => {
+      const { name, daily_rate } = entry.employee;
+
+      let employeeData = acc.find(e => e.name === name);
+
+      if (!employeeData) {
+        employeeData = { name, daily_rate, work_days: [] };
+        acc.push(employeeData);
+      }
+      employeeData.work_days.push({
+        hours_worked: entry.hours_worked,
+        created_at: entry.created_at,
+      });
+
+      return acc;
+  }, [] as { name: string; daily_rate: number; work_days: { hours_worked: number; created_at: Date }[] }[]);
+  return groupedReport;
   },
 };
